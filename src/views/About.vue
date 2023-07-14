bae309228b5e438fe37d199dd25dae97d2bd4bd9
kkopoku <kk.opoku@outlook.com>
2023-07-14 03:46:09 +0000

    small change

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>About Us</h1>
-        <p>This is the about page content.</p>
+        <p>This is the about page content for my rigelis test work.</p>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4" @click="goToHome">Go to
             Home
         </button>
